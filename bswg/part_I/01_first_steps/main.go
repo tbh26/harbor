a5af8e8ec6fba2f3bdece5a96537c0e953b45173
Tjeerd Hes <tjeerd.hes@icloud.com>
2023-08-05 10:50:47 +0200

    [bswg][p1-02] setup

--- bswg/part_I/01_first_steps/main.go
+++ bswg/part_I/01_first_steps/main.go
@@ -1,11 +1,38 @@
 package main
 
-import "fmt"
-
-func hello() {
-	fmt.Println("Build systems wirt GO, save the world.")
-}
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
 	hello()
+	inspectArgs()
+	argsAdd()
+}
+
+func hello() {
+	fmt.Println("Build systems with GO, save the world.")
+}
+
+func inspectArgs() {
+	fmt.Println()
+	args := os.Args
+	for index, arg := range args {
+		fmt.Printf(" [%d], arg: '%s' \n", index, arg)
+	}
+	fmt.Printf("\n%V\n\n", args)
+}
+
+func argsAdd() {
+	args := os.Args[1:]
+	total := 0
+	for _, arg := range args {
+		n, e := strconv.Atoi(arg)
+		if e == nil {
+			total += n
+		}
+	}
+	fmt.Printf("\n total (sum) == %d \n", total)
 }
---
+++ bswg/part_I/02_basics/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	hello()
+}
+
+func hello() {
+	fmt.Println("Build systems with GO, save the world. (part 1, chapter 2")
+}
