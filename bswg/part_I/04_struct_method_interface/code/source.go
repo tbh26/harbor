9e448357e575ae336aba8a0c17b2b12f104917ed
Tjeerd Hes <tjeerd.hes@amsteldiep.nl>
2023-08-07 10:39:32 +0200

    [bswg][p1-04] demo struct

---
+++ bswg/part_I/04_struct_method_interface/code/source.go
@@ -0,0 +1,96 @@
+package code
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+func Demo() {
+	fmt.Println("Hello code.Demo() world! (ch4)")
+	//
+	structDemo()
+	methodDemo()
+	interfaceDemo()
+	//
+	fmt.Println()
+}
+
+type Item struct {
+	Topic       string
+	Description string
+	Code        int64
+}
+
+func NewItem(topic string, desciption string, code int64) (*Item, error) {
+	if len(topic) == 0 || code < 0 {
+		return nil, errors.New("invalid input(s)/param(s)")
+	}
+	return &Item{topic, desciption, code}, nil
+}
+
+type DayOfBirth struct {
+	Day   int8
+	Month int8
+	Year  int
+}
+
+type Person struct {
+	FullName string
+	Dob      DayOfBirth // nested struct
+}
+
+type Person2 struct {
+	FullName   string
+	DayOfBirth // embedded struct (as long as uniq)
+}
+
+func structDemo() {
+	fmt.Println()
+	//
+	i1 := Item{}
+	fmt.Printf(" i1 == %+v \n", i1)
+	//
+	i2 := Item{"chair", "", 12345}
+	i2.Code = 54321
+	fmt.Printf(" i2 == %v \n", i2)
+	//
+	i3 := Item{Topic: "table", Description: "workdesk", Code: 98765}
+	fmt.Printf(" i3 == %+v \n", i3)
+	//
+	i4 := Item{Topic: "Closet"}
+	fmt.Printf(" i4 == %v \n", i4)
+	//
+	//	i5, e := NewItem("", "", -12) // panics!
+	i5, e := NewItem("stool", "n", 12)
+	if e != nil {
+		panic(e)
+	}
+	fmt.Printf(" *i5 == %+v \n", *i5)
+	//
+	//anonymous struct
+	as := struct {
+		label string
+		val   int
+	}{"thing", 42}
+	fmt.Printf(" as == %+v \n", as)
+	//
+	fmt.Printf(" var 'i4' type-of '%s', var 'as' type-of '%s' \n", reflect.TypeOf(i4), reflect.TypeOf(as))
+	//
+	dob := DayOfBirth{21, 3, 1987}
+	name := "John Doe"
+	jd := Person{name, dob}
+	fmt.Printf(" jd == %+v \n", jd)
+	p2 := Person2{name, dob}
+	fmt.Printf(" p2 == %+v \n", p2)
+}
+
+func methodDemo() {
+	fmt.Println()
+	//
+}
+
+func interfaceDemo() {
+	fmt.Println()
+	//
+}
---
+++ bswg/part_I/04_struct_method_interface/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "example.org/bswg/p1/ch4/code"
+
+func main() {
+	code.Demo()
+}
