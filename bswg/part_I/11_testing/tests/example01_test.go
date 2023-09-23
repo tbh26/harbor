3e64c2bbabfd2428ffb1678b75efa0b782698b9a
Tjeerd Hes <tjeerd.hes@icloud.com>
2023-09-23 10:37:06 +0200

    [bswg][p1-11] tests, example01

---
+++ bswg/part_I/11_testing/tests/example01_test.go
@@ -0,0 +1,27 @@
+package example_01
+
+// % go test  -v
+// % go fmt  ./...
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test01(t *testing.T) {
+	expected := 4
+	r := 2 + 2
+	if r != expected {
+		t.Errorf("Expected %d, got %d.\n", expected, r)
+	} else {
+		fmt.Printf("Expected %d, and got %d, nice!\n", expected, r)
+	}
+}
+
+func Test02(t *testing.T) {
+	notExpected := 42
+	r := 2 + 2
+	if r == notExpected {
+		t.Errorf("NOT expected %d, and got %d.\n", notExpected, r)
+	}
+}
