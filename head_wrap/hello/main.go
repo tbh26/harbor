143c31bdd4421468a9255f25cc9b6a95e0a3f2e8
Tjeerd Hes <tjeerd.hes@icloud.com>
2024-01-05 22:55:13 +0100

    [head_wrap] demo vars

--- head_wrap/hello/main.go
+++ head_wrap/hello/main.go
@@ -6,6 +6,40 @@ package main
 
 import "fmt"
 
+var (
+	Author  = "tbh"
+	comment = "bla bla"
+)
+
+const (
+	Version = "0.1.2"
+	greet   = "howdy 👋"
+)
+
+func demoVars() {
+	var message string = "Hello demo vars"
+	fmt.Println(message)
+
+	var x, y, z int = 2, 40, 42
+	fmt.Printf("x: %d, y: %d, z: %d \n", x, y, z)
+
+	var s1 = "var without type, using type inference"
+	s2 := "short variable declaration syntax demo"
+	fmt.Printf("s1: %s, s2: %q \n", s1, s2)
+
+	e, d, f := 123, 456.789, true
+	fmt.Printf("e: %v, d: %.2f, f: %t \n", e, d, f)
+
+	explain := "using globals, uppercase global var will be exported"
+	comment = "bla"
+	fmt.Printf("Author: %#v, comment: %q  (%s) \n", Author, comment, explain)
+	fmt.Printf("Version: %q, greet: %q \n", Version, greet)
+}
+
 func main() {
+	fmt.Println()
 	fmt.Println("Hello head wrapping go...")
+	fmt.Println()
+
+	demoVars()
 }
