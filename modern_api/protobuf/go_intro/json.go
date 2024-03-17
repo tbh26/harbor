0ad15d168cbac95bdaa86979dd25cc085dc67ade
Tjeerd Hes <tjeerd.hes@amsteldiep.nl>
2024-03-17 10:47:43 +0100

    [protobuf][protoc] add to/from json conversions

---
+++ modern_api/protobuf/go_intro/json.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+)
+
+func toJSON(pb proto.Message) string {
+	option := &protojson.MarshalOptions{
+		Multiline: true,
+	}
+
+	out, err := option.Marshal(pb)
+	if err != nil {
+		log.Fatalln("toJSON(); can't convert to JSON", err)
+		return ""
+	}
+
+	return string(out)
+}
+
+func fromJSON(in string, pb proto.Message) {
+	option := protojson.UnmarshalOptions{
+		DiscardUnknown: true,
+	}
+
+	if err := option.Unmarshal([]byte(in), pb); err != nil {
+		log.Fatalln("fromJSON(); couldn't invert from JSON", err)
+	}
+}
