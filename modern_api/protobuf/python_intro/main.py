4472e7790e8dd56c1766a3de13ae879f83dc403d
Tjeerd Hes <tjeerd.hes@amsteldiep.nl>
2024-03-17 15:37:04 +0100

    [protobuf/c][python3] add 'simple' demo to main

--- modern_api/protobuf/python_intro/main.py
+++ modern_api/protobuf/python_intro/main.py
@@ -1,9 +1,27 @@
 #! /usr/bin/env python3
 ###
+from proto import simple_pb2
+
+
+def create_simple() -> simple_pb2.Simple:
+    return simple_pb2.Simple(
+        id=42,
+        is_simple=True,
+        name="My python3 simple proto name",
+        sample_lists=[3, 4, 5]
+    )
+
+
+def simple_demo():
+    simple = create_simple()
+    print(f'{simple=}')
+    print()
+
 
 def main():
     print('Hello python proto world!')
     print()
+    simple_demo()
 
 
 if __name__ == '__main__':
