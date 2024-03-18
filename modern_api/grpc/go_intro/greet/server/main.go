9988f374911cfcc5e9a39dd42f50c9f8332e9a8f
Tjeerd Hes <tjeerd.hes@amsteldiep.nl>
2024-03-18 11:31:00 +0100

    [grpc][go intro][greet] server setup

---
+++ modern_api/grpc/go_intro/greet/server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"log"
+	"net"
+
+	pb "github.com/tbh26/harbor/modern_api/grpc/go_intro/greet/proto"
+)
+
+var address string = "0.0.0.0:40041"
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	l, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("listen failed (%q); %v \n", address, err)
+	}
+	log.Printf("listing on %q \n", address)
+	s := grpc.NewServer()
+	if err = s.Serve(l); err != nil {
+		log.Fatalf("failed to serve: %v \n", err)
+	}
+}
