b80d8fc9c73b2f59629c3b2ee2947fb86dc955fc
Tjeerd Hes <tjeerd.hes@amsteldiep.nl>
2024-03-29 12:23:21 +0100

    [concurrency][go intro] challenge (in order)

--- concurrent_go/udemy/intro/main_test.go
+++ concurrent_go/udemy/intro/main_test.go
@@ -26,3 +26,20 @@ func Test_printSome(t *testing.T) {
 		t.Errorf(em)
 	}
 }
+
+func Test_challenge(t *testing.T) {
+	stdOut := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	words := []string{"foo", "bar"}
+	challenge(words)
+	_ = w.Close()
+	output, _ := io.ReadAll(r)
+	os.Stdout = stdOut // restore standard out
+	for _, word := range words {
+		if !strings.Contains(string(output), word) {
+			em := fmt.Sprintf("Excepted %q in the output.", word)
+			t.Errorf(em)
+		}
+	}
+}
