a0d182ddd342662a768a6b296f1a4eda7df6b7f0
Tjeerd Hes <tjeerd.hes@amsteldiep.nl>
2024-03-29 10:46:47 +0100

    [concurrency][go intro] first demo

--- concurrent_go/udemy/intro/main.go
+++ concurrent_go/udemy/intro/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func printSomething(s string) {
@@ -11,20 +12,20 @@ func printSomething(s string) {
 func firstDemo() {
 	// if you run the program with this line uncommented, and the lines 20 commented,
 	// everything works as expected
-	printSomething("This is the first thing to be printed!")
+	go printSomething("This may be the first thing to be printed!")
 
 	// but if you comment out line 15 and uncomment the one below this comment,
 	// running the program will (probably) just print out the final message,
 	// since the program terminates before the goroutine started by this
 	// command does not have time to finish.
-	//go printSomething("This is the first thing to be printed!")
+	go printSomething("This may be the next thing to be printed!")
 
 	// in order to give the goroutine from line 20 time to finish, we could
 	// wait for second by uncommenting the line below, but this is hardly
 	// a good solution.
-	//time.Sleep(1 * time.Second)
+	time.Sleep(5 * time.Millisecond)
 
-	printSomething("This is the second thing to be printed!")
+	printSomething("This should be the last thing to be printed!")
 }
 
 func main() {
@@ -33,4 +34,5 @@ func main() {
 	fmt.Println()
 
 	firstDemo()
+	fmt.Println()
 }
