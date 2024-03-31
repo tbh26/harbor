a9ce1ec7e4964fd05743934d4ccd319d182c86b7
Tjeerd Hes <tjeerd.hes@amsteldiep.nl>
2024-03-31 22:38:11 +0200

    [concurrency][manning book][ch 3] setup character/letter frequency counter

---
+++ concurrent_go/manning_book/chapter_3/character_frequency/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+const allLetters = "abcdefghijklmnopqrstuvwxyz"
+
+func countLetters(url string, frequency []int) {
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		panic("Server returning error status code: " + resp.Status)
+	}
+	body, _ := io.ReadAll(resp.Body)
+	for _, b := range body {
+		c := strings.ToLower(string(b))
+		cIndex := strings.Index(allLetters, c)
+		if cIndex >= 0 {
+			frequency[cIndex] += 1
+		} // else skip
+	}
+	fmt.Println("Completed:", url)
+}
+
+func main() {
+	var frequency = make([]int, len(allLetters))
+	for i := 1000; i <= 1030; i++ {
+		url := fmt.Sprintf("https://www.rfc-editor.org/rfc/rfc%d.txt", i)
+		countLetters(url, frequency)
+	}
+	for i, c := range allLetters {
+		fmt.Printf(" %c - %d \n", c, frequency[i])
+	}
+}
