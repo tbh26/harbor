0f196c1e2267f4d6dfa03674326bde2c12c6075f
Tjeerd Hes <tjeerd.hes@amsteldiep.nl>
2024-04-01 10:34:35 +0200

    [concurrency][manning book][ch 4] stingy spendy updated

--- concurrent_go/manning_book/chapter_3/stingy_spendy/main.go
+++ concurrent_go/manning_book/chapter_3/stingy_spendy/main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
 
-func stingy(money *int) {
+func stingyInitial(money *int) {
 	for i := 0; i < 1000000; i++ {
 		*money += 10
 	}
 	fmt.Println("Stingy Done")
 }
 
-func spendy(money *int) {
+func spendyInitial(money *int) {
 	for i := 0; i < 1000000; i++ {
 		*money -= 10
 	}
@@ -21,13 +22,40 @@ func spendy(money *int) {
 
 func initialDemo() {
 	money := 100
-	go stingy(&money)
-	go spendy(&money)
+	go stingyInitial(&money)
+	go spendyInitial(&money)
 	time.Sleep(2 * time.Second)
 	println("Money in bank account: ", money)
 }
 
+func stingy2(money *int) {
+	for i := 0; i < 1000000; i++ {
+		*money += 10
+		runtime.Gosched()
+	}
+	fmt.Println("Stingy runtime.Gosched() Done")
+}
+
+func spendy2(money *int) {
+	for i := 0; i < 1000000; i++ {
+		*money -= 10
+		runtime.Gosched()
+	}
+	fmt.Println("Spendy runtime.Gosched() Done")
+}
+
+func demo2() {
+	money := 100
+	go stingy2(&money)
+	go spendy2(&money)
+	time.Sleep(2 * time.Second)
+	println("Money in bank account (2): ", money)
+}
+
 func main() {
 	fmt.Println("Hello stingy & spendy world 🧐")
+
 	initialDemo()
+
+	demo2()
 }
