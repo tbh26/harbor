25750b8571938f45811999d5b6f48162310f2a6d
Tjeerd Hes <tjeerd.hes@icloud.com>
2024-04-06 13:36:58 +0200

    [concurrency][manning book][ch 5] update semaphore / demo

---
+++ concurrent_go/manning_book/chapter_5/semaphore/semaphore.go
@@ -0,0 +1,31 @@
+package semaphore
+
+import "sync"
+
+type Semaphore struct {
+	permits int
+	cond    *sync.Cond
+}
+
+func NewSemaphore(n int) *Semaphore {
+	return &Semaphore{
+		permits: n,
+		cond:    sync.NewCond(&sync.Mutex{}),
+	}
+}
+
+func (rw *Semaphore) Acquire() {
+	rw.cond.L.Lock()
+	for rw.permits <= 0 {
+		rw.cond.Wait()
+	}
+	rw.permits--
+	rw.cond.L.Unlock()
+}
+
+func (rw *Semaphore) Release() {
+	rw.cond.L.Lock()
+	rw.permits++
+	rw.cond.Signal()
+	rw.cond.L.Unlock()
+}
--- concurrent_go/manning_book/chapter_5/semaphore_demo/main.go
+++ concurrent_go/manning_book/chapter_5/semaphore_demo/main.go
@@ -2,45 +2,17 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"github.com/tbh26/harbor/concurrent_go/manning_book/chapter_5/semaphore"
 )
 
-type Semaphore struct {
-	permits int
-	cond    *sync.Cond
-}
-
-func NewSemaphore(n int) *Semaphore {
-	return &Semaphore{
-		permits: n,
-		cond:    sync.NewCond(&sync.Mutex{}),
-	}
-}
-
-func (rw *Semaphore) Acquire() {
-	rw.cond.L.Lock()
-	for rw.permits <= 0 {
-		rw.cond.Wait()
-	}
-	rw.permits--
-	rw.cond.L.Unlock()
-}
-
-func (rw *Semaphore) Release() {
-	rw.cond.L.Lock()
-	rw.permits++
-	rw.cond.Signal()
-	rw.cond.L.Unlock()
-}
-
-func doWork(semaphore *Semaphore) {
+func doWork(semaphore *semaphore.Semaphore) {
 	fmt.Println("Work started")
 	fmt.Println("Work finished")
 	semaphore.Release()
 }
 
 func demo() {
-	semaphore := NewSemaphore(0)
+	semaphore := semaphore.NewSemaphore(0)
 	for i := 0; i < 50000; i++ {
 		go doWork(semaphore)
 		fmt.Println("Waiting for child goroutine")
