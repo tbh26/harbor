d23b0d013b6a2b6ccebf50e1fc7792b1df6c1dd5
Tjeerd Hes <tjeerd.hes@icloud.com>
2024-04-06 19:21:56 +0200

    [concurrency][manning book][ch 7] demo broken channel with receiveTwice

--- concurrent_go/manning_book/chapter_7/channels_intro/main.go
+++ concurrent_go/manning_book/chapter_7/channels_intro/main.go
@@ -14,7 +14,19 @@ func receiver(tag string, messages chan string) {
 	fmt.Printf("End receiver %q \n", tag)
 }
 
-func demo() {
+func receiveTwice(tag string, messages chan string) {
+	// receive two messages and call it a day
+
+	msg := <-messages
+	fmt.Printf("Message [%q] received; %q \n", tag, msg)
+
+	msg = <-messages
+	fmt.Printf("Message [%q] received; %q \n", tag, msg)
+
+	fmt.Printf("End receiver %q \n", tag)
+}
+
+func firstDemo() {
 	msgChannel := make(chan string)
 	go receiver("first channel", msgChannel)
 	messages := []string{"Hello...", "channel", "world!", "STOP"}
@@ -23,7 +35,27 @@ func demo() {
 		msgChannel <- message
 	}
 	time.Sleep(321 * time.Microsecond)
-	fmt.Println("\n - exit demo... \n")
+	fmt.Println("\n - exit first demo... \n")
+}
+
+func nextDemo() {
+	msgChannel := make(chan string)
+	go receiveTwice("broken channel receiver", msgChannel)
+	messages := []string{"bla", "bla bla", "whatever...", "STOP bla"}
+	for _, message := range messages {
+		fmt.Printf("Sending message %q into channel.\n", message)
+		msgChannel <- message
+	}
+	time.Sleep(321 * time.Microsecond)
+	fmt.Println("\n - exit next/last demo.\n")
+}
+
+func demo() {
+	firstDemo()
+
+	fmt.Println("\n\t\t=-=-=-=\n")
+
+	nextDemo()
 }
 
 func main() {
