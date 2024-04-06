a06cb8d1810c9039abf7f0139adfff7444d26af3
Tjeerd Hes <tjeerd.hes@icloud.com>
2024-04-06 23:16:57 +0200

    [concurrency][manning book][ch 7] close transmitter channel demo

--- concurrent_go/manning_book/chapter_7/channel_direction_demo/main.go
+++ concurrent_go/manning_book/chapter_7/channel_direction_demo/main.go
@@ -8,41 +8,67 @@ import (
 )
 
 const (
-	lastMessage  = "Stop!"
-	emptyMessage = ""
-	nowFormat    = "15:04:05"
-	bufferSize   = 3
+	lastMessage = "Stop!"
+	nowFormat   = "15:04:05"
+	bufferSize  = 3
 )
 
 func slowReceiver(tag string, messages <-chan string, wg *sync.WaitGroup) {
-	msg := emptyMessage
+	var msg string
+	var ok bool
 	for msg != lastMessage {
-		msg = <-messages
+		msg, ok = <-messages
 		time.Sleep(1 * time.Second)
-		fmt.Printf("[%s] Message %q received (at: %s).\n", tag, msg, time.Now().Format(nowFormat))
+		fmt.Printf("[%s] Message %q received (at: %s, more: %v).\n", tag, msg, time.Now().Format(nowFormat), ok)
+		if !ok {
+			fmt.Println("(slow receiver) NO lastMessage, but channel seems closed, break-up.")
+			break
+		}
 	}
+	// note: a receiving channel can not be closed
+	// close(messages) // close can only be called on a bidirectional or send-only channel
 	wg.Done()
 }
 
-func transmitter(tag string, messageChannel chan<- string, wg *sync.WaitGroup) {
+func transmitter(tag string, msgSrc string, messageChannel chan<- string, wg *sync.WaitGroup) {
 	fmt.Printf("Sender/transmitter %q start at %s.\n", tag, time.Now().Format(nowFormat))
-	messagesString := fmt.Sprintf("Send or transmit into channels with direction demo. %s", lastMessage)
-	for i, message := range strings.Split(messagesString, " ") {
+	for i, message := range strings.Split(msgSrc, " ") {
 		fmt.Printf("Move message[%d] %q into channel (at %s).\n",
 			i, message, time.Now().Format(nowFormat))
 		messageChannel <- message
 	}
+	close(messageChannel)
 	wg.Done()
 }
 
-func demo() {
+func firstDemo() {
 	msgChannel := make(chan string, bufferSize)
 	waitGroup := sync.WaitGroup{}
 	waitGroup.Add(2)
+	messagesSource := fmt.Sprintf("Send or transmit into channels with direction demo. %s", lastMessage)
 	go slowReceiver("(slow) receiver", msgChannel, &waitGroup)
-	go transmitter("generate words", msgChannel, &waitGroup)
+	go transmitter("generate words", messagesSource, msgChannel, &waitGroup)
 	waitGroup.Wait()
 	fmt.Printf("Done waiting, at %s (on channel with buffer size %d).\n", time.Now().Format(nowFormat), bufferSize)
+}
+
+func nextDemo() {
+	msgChannel := make(chan string, bufferSize)
+	waitGroup := sync.WaitGroup{}
+	waitGroup.Add(2)
+	messagesSource := fmt.Sprintf("NO last_message here just nonsense %s", nowFormat)
+	go slowReceiver("(slow) receiver", msgChannel, &waitGroup)
+	go transmitter("generate words", messagesSource, msgChannel, &waitGroup)
+	waitGroup.Wait()
+	fmt.Printf("Done waiting, at %s (on channel with buffer size %d).\n", time.Now().Format(nowFormat), bufferSize)
+}
+
+func demo() {
+	firstDemo()
+
+	fmt.Println("\n\t=-=-=\n")
+
+	nextDemo()
 }
 
 func main() {
