681694442adf4dcd6810645860b2153f258cc1c9
Tjeerd Hes <tjeerd.hes@icloud.com>
2024-04-08 13:31:32 +0200

    [concurrency][manning book][ch 8] use select; password guess demo

--- concurrent_go/manning_book/chapter_8/select_intro/main.go
+++ concurrent_go/manning_book/chapter_8/select_intro/main.go
@@ -47,6 +47,64 @@ func firstDemo(log *slog.Logger) {
 	log.Debug(" end  firstDemo")
 }
 
+////
+
+const (
+	passwordToGuess = "go far"
+	alphabet        = " abcdefghijklmnopqrstuvwxyz"
+)
+
+func toBase27(n int) string {
+	result := ""
+	for n > 0 {
+		result = string(alphabet[n%27]) + result
+		n /= 27
+	}
+	return result
+}
+
+func guessPassword(from int, upto int, stop chan int, result chan string) {
+	for guessN := from; guessN < upto; guessN += 1 {
+
+		select {
+
+		case <-stop:
+			fmt.Printf("Stopped at %d [%d, %d]\n", guessN, from, upto-1)
+			return
+
+		default:
+			if toBase27(guessN) == passwordToGuess {
+				result <- toBase27(guessN)
+				//fmt.Println("guessN:", guessN)
+				close(stop)
+				return
+			}
+		}
+	}
+	fmt.Printf("Not found between [%d, %d]\n", from, upto-1)
+}
+
+func fictitiousPasswordGuessDemo(log *slog.Logger) {
+	log.Debug("begin fictitiousPasswordGuessDemo")
+
+	finished := make(chan int)
+	passwordFound := make(chan string)
+	for i := 1; i <= 387_420_488; i += 10_000_000 {
+		go guessPassword(i, i+10_000_000, finished, passwordFound)
+	}
+
+	fmt.Printf("fictitiousPasswordGuessDemo, password found: %q \n", <-passwordFound)
+	close(passwordFound)
+	time.Sleep(1 * time.Second) // why?
+
+	magicN := 108418383
+	fmt.Printf("magic N: %d, toBase27(magicN): %q \n", magicN, toBase27(magicN))
+
+	log.Debug(" end  fictitiousPasswordGuessDemo")
+}
+
+////
+
 func demo() {
 	slogOpts := slog.HandlerOptions{
 		//Level: slog.LevelDebug,
@@ -57,7 +115,7 @@ func demo() {
 
 	fmt.Println("\n\t\t=-=-=-=\n")
 
-	// nextDemo()
+	fictitiousPasswordGuessDemo(logger)
 }
 
 func main() {
